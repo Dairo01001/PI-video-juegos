0f76cc4fd1c9ee3e2d6ad16fdc9014d017de02bb
Dairo01001 <garcianaranjodairo@gmail.com>
2022-01-17 18:26:41 -0500

    GameDetail

--- client/src/components/GameDetail/index.jsx
+++ client/src/components/GameDetail/index.jsx
@@ -12,14 +12,35 @@ const GameDetail = () => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  const { name, background_image} = useSelector(
-    (state) => state.gameDetail
-  );
+  const gameDetail = useSelector((state) => state.gameDetail);
+
+  const {
+    name,
+    background_image,
+    genres,
+    description,
+    released,
+    ratings,
+  } = gameDetail;
 
   return (
     <div>
       <h2>{name}</h2>
       <img src={background_image} alt={name} />
+      <time dateTime={released}>{released?.replaceAll("-", "/")}</time>
+      <ul>
+        {genres?.map((genre) => (
+          <li key={genre.id}>{genre.name}</li>
+        ))}
+      </ul>
+      <div dangerouslySetInnerHTML={{ __html: description }}></div>
+      <ul>
+        {ratings?.map(({ id, title, percent }) => (
+          <p key={id}>
+            {title}: {percent}%
+          </p>
+        ))}
+      </ul>
     </div>
   );
 };
