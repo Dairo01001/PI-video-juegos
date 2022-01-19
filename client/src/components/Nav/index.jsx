fdd063861eb290953403c0d15609cfdeacce06d7
Dairo01001 <garcianaranjodairo@gmail.com>
2022-01-18 21:23:44 -0500

    SearchBar

--- client/src/components/Nav/index.jsx
+++ client/src/components/Nav/index.jsx
@@ -1,10 +1,12 @@
 import { Link } from "react-router-dom";
+import SearchBar from "../SearchBar";
 import styled from "./Nav.module.css";
 
 const Nav = () => {
   return (
     <header className={styled.head}>
-      <h1>Video Juegos</h1>
+      <h1>VIDEO GAMES</h1>
+      <SearchBar />
       <nav>
         <Link to="/home/creategame">Create Game</Link>
       </nav>
---
+++ client/src/components/SearchBar/index.jsx
@@ -0,0 +1,13 @@
+
+const SearchBar = () => {
+  return (
+    <section>
+      <form>
+        <input type="search"></input>
+        <input type="submit" value="&#10140;"></input>
+      </form>
+    </section>
+  );
+};
+
+export default SearchBar;
